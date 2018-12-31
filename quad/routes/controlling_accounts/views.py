1241a0272a9be913ca75b022047015302fc875e4
Vicente Chavez <vchavez92780@gmail.com>
2018-12-31 10:47:11 -0500

    Updated blueprint to work with the latest Flask version

--- quad/routes/controlling_accounts/views.py
+++ quad/routes/controlling_accounts/views.py
@@ -4,10 +4,10 @@ from quad.utils.views import execute_sp_func_from_view
 from .api_config import path_sp_args_map
 
 
-blueprint = Blueprint('controlling_accounts', __name__)
+blueprint = Blueprint('controlling_accounts', __name__, url_prefix='/api/controlling_accounts')
 
 
-@blueprint.route('/api/controlling_accounts/<path:path>', methods=('GET',))
+@blueprint.route('/<path:path>', methods=('GET',))
 @nocache
 def get_sp_data(path):
     return execute_sp_func_from_view(path, path_sp_args_map)
