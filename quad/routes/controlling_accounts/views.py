8f7e292152300f50ccee1217f8945908888a9ea3
Vicente Chavez <vchavez92780@gmail.com>
2019-01-02 11:28:21 -0500

    Accept default path

--- quad/routes/controlling_accounts/views.py
+++ quad/routes/controlling_accounts/views.py
@@ -7,6 +7,7 @@ from .api_config import path_sp_args_map
 blueprint = Blueprint('controlling_accounts', __name__, url_prefix='/api/controlling_accounts')
 
 
+@blueprint.route('/', defaults={'path': 'all'})
 @blueprint.route('/<path:path>', methods=('GET',))
 @nocache
 def get_sp_data(path):
