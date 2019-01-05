0a0f9ffe2ad184d67d2a3592c7d4e0ae22f7e342
Vicente Chavez <vchavez92780@gmail.com>
2019-01-05 12:56:15 -0500

    Configure route for creating controlling accounts

--- quad/routes/controlling_accounts/api_config.py
+++ quad/routes/controlling_accounts/api_config.py
@@ -1,9 +1,17 @@
 path_sp_args_map = {
-  'all': {
-    'module_name': 'quad.utils.db',
-    'module_func': 'execute_quad_sp',
-    'sp_func': 'execute_admin_console_sp',
-    'sp_name': 'QUAD.MANAGE_CTRLACCT',
-    'sp_message': 'DISPLAY ALL'
-  }
+    'all': {
+        'module_name': 'quad.utils.db',
+        'module_func': 'execute_quad_sp',
+        'sp_name': 'QUAD.MANAGE_CTRLACCT',
+        'sp_message': 'DISPLAY ALL',
+        'request_type': 'GET'
+    },
+    'create': {
+        'module_name': 'quad.db.controlling_accounts',
+        'module_func': 'create',
+        'sp_name': 'QUAD.MANAGE_CTRLACCT',
+        'sp_message': 'ADD',
+        'sp_in_args': ['code', 'name', 'p_ctrl_acct_id', 'n_level'],
+        'request_type': 'POST'
+    }
 }
