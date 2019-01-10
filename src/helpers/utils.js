4ac17bcf71c0314cb0dd93937ebd9b10875fec92
Vicente Chavez <vchavez92780@gmail.com>
2019-01-09 21:19:51 -0500

    Fixed bug

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -8,13 +8,18 @@
 export const getListIndicesLookupIdx = (list, keyProps = 'id') => {
   const lookupIdx = {};
   if (typeof keyProps === 'string') {
+    lookupIdx[keyProps] = {};
     list.forEach((item, i) => {
       lookupIdx[keyProps][item[keyProps]] = i;
     });
   } else {
+    keyProps.forEach((keyProp) => {
+      lookupIdx[keyProp] = {};
+    });
+
     list.forEach((item, i) => {
       keyProps.forEach((keyProp) => {
-        lookupIdx[keyProp][item[keyProps]] = i;
+        lookupIdx[keyProp][item[keyProp]] = i;
       });
     });
   }
