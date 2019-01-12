a61926be41b397a0b17e58683c589691f4e93ead
Vicente Chavez <vchavez92780@gmail.com>
2019-01-11 20:54:08 -0500

    Use event wrappers from my js package

--- src/routes/ControllingAccounts/SelectionNode/SelectionNode.js
+++ src/routes/ControllingAccounts/SelectionNode/SelectionNode.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { submitEvent } from 'react-virtualized-tree/lib/eventWrappers';
+import { submitEvent } from 'javascript-utils/lib/eventWrappers';
 
 export const SELECT = 3;
 
